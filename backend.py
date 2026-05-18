fb1d6f4746b65cfd81a6279fe31de19cf493449a
Kilian Imme <selbstmord234@gmail.com>
2026-05-18 14:28:41 +0200

    `Update MariaDB host IP address in app.py and backend.py`

--- backend.py
+++ backend.py
@@ -14,7 +14,7 @@ from paho.mqtt.client import CallbackAPIVersion
 # ---------------------------------------------------------------------------
 # Config via environment variables
 # ---------------------------------------------------------------------------
-os.environ.setdefault("MARIADB_HOST",     "127.0.0.1")
+os.environ.setdefault("MARIADB_HOST",     "172.21.0.2")
 os.environ.setdefault("MARIADB_PORT",     "3306")
 os.environ.setdefault("MARIADB_USER",     "dashboard1")
 os.environ.setdefault("MARIADB_PASSWORD", "dashpw")
